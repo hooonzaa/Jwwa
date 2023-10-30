3a21628ef7072775f2207896300fe65339f3fcda
hooonzaa <145441106+hooonzaa@users.noreply.github.com>
2023-10-30 14:58:39 +0100

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,80 @@
+function Pythagorova() {
+
+    const numberA = parseFloat(document.getElementById('a').value)
+    const numberB = parseFloat(document.getElementById('b').value)
+
+    let.result = numberA
+
+}
+if (!isNaN) (a) && (!isNaN)(b)
+
+
+
+Math.sqrt('c')
+
+
+if (true) {
+    console.log('Ahoj')
+}
+
+function program() {
+    if (!10) (10)
+    Let.gettresoult('F')
+
+
+
+}
+function Ověření() {
+    const inputAge = parseFloat(document.getElementById('18×').value)
+    if (inputAge >= 18) {
+        console.log('Dospělí uživatel')
+    }
+    else if (inputAge < 18) {
+        console.log('Smol')
+    }
+}
+function Rokchecker() {
+
+    const inputRok = parseFloat(document.getElementById('Rok').value)
+    if (inputRok < 2005) {
+        document.getElementById('out').value = "Dospělí"
+
+    }
+    else if (inputRok >= 2005) {
+        document.getElementById('out').value = "dite"
+
+    }
+
+}
+function checker() {
+
+    let inputRok2 = parseFloat(document.getElementById('Rok2').value)
+
+    inputRok2 = inputRok2 + 10
+    document.getElementById('OP2').innerHTML = inputRok2
+
+}
+
+
+function acker() {
+
+    const inputRok3 = parseFloat(document.getElementById('Rok3').value)
+    
+    if (inputRok3 >= 18 && inputRok3 <= 2005)
+       
+    
+    document.getElementById('out').value = "Dospělí"
+    
+    
+    else
+
+
+        document.getElementById('out').value = "dite"
+
+
+
+
+
+
+
+}
